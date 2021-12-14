90ca65166c265a6af4fbc4c096b6e389a9e8ff39
Tony Francis <tonyfrancisv@gmail.com>
2021-12-14 16:46:49 -0500

    imports

--- {{cookiecutter.project_slug}}/backend/app/app/data/user/manage.py
+++ {{cookiecutter.project_slug}}/backend/app/app/data/user/manage.py
@@ -1,20 +1,18 @@
 from typing import Optional
 
-from data import crud
-from data.user.schemas import User, UserAuthCreate, UserCreate
-from data.auth.schemas import AuthCreate
+from data import crud, schemas
 from sqlalchemy.orm import Session
 from app.core.security import  verify_password
 
 class ManageUser:
     @staticmethod
-    async def create_user(db: Session, obj_in: UserAuthCreate) -> User:
+    async def create_user(db: Session, obj_in: schemas.UserAuthCreate) -> schemas.User:
         auth = await crud.auth.create(
-            db, obj_in=AuthCreate(email=obj_in.email, password=obj_in.password)
+            db, obj_in=schemas.AuthCreate(email=obj_in.email, password=obj_in.password)
         )
         user = await crud.user.create(
             db,
-            obj_in=UserCreate(
+            obj_in=schemas.UserCreate(
                 auth_id=auth.id,
                 first_name=obj_in.first_name,
                 last_name=obj_in.last_name,
@@ -25,7 +23,7 @@ class ManageUser:
         return user
 
     @staticmethod
-    async def authenticate(db: Session, *, email: str, password: str) -> Optional[User]:
+    async def authenticate(db: Session, *, email: str, password: str) -> Optional[schemas.User]:
         user = crud.user.get_by_email(db, email=email)
         if not user:
             return None
